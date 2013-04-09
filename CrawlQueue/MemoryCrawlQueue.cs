7052abcafeb6a0e8c69a4da3355a8550c9a38eec
Lewis Ellington <lewis.ellington@gmail.com>
2013-04-09 11:15:22 +0100

    Memory crawl queue

---
+++ CrawlQueue/MemoryCrawlQueue.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Crawler
+{
+    public class MemoryCrawlQueue : ICrawlQueue
+    {
+        private ConcurrentDictionary<string, byte> m_visitedUris = new ConcurrentDictionary<string, byte>();
+        private ConcurrentQueue<Uri> m_crawlQueue = new ConcurrentQueue<Uri>();
+
+        public int UrisRemaining
+        {
+            get { return m_crawlQueue.Count; }
+        }
+
+        public void Enqueue(Uri uri)
+        {
+            if (uri == null)
+            {
+                throw new ArgumentNullException("uri");
+            }
+
+            m_crawlQueue.Enqueue(uri);
+        }
+
+        public Uri GetNext()
+        {
+            Uri uri = default(Uri);
+
+            m_crawlQueue.TryDequeue(out uri);
+
+            return uri;
+        }
+    }
+}
